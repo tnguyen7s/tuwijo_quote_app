01d78113a39970ad083a33f6c8b26a0429fa0cf3
Thi Thanh Tuyen Nguyen <tnguyen7s@semo.edu>
2022-06-03 00:05:37 -0500

    create quote-list and quote-card

---
+++ src/app/shared/quote-card/quote-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuoteCardComponent } from './quote-card.component';
+
+describe('QuoteCardComponent', () => {
+  let component: QuoteCardComponent;
+  let fixture: ComponentFixture<QuoteCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ QuoteCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuoteCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
