27b1c91f08a8abddc1658b22c6f9ec5ef14652aa
GeeWilleh <geewilleh@gmail.com>
2022-06-08 23:51:43 -0500

    First API test
    not working yet, adding httpClientModule and beginging steps to test api

---
+++ src/app/constants.ts
@@ -0,0 +1,23 @@
+// Angular Modules
+import { Injectable } from '@angular/core';
+@Injectable()
+export class constants {
+public readonly API_ENDPOINT: string = 'https://andruxnet-random-famous-quotes.p.rapidapi.com/?count=1';
+}
+import { Component, OnInit } from '@angular/core';
+import{ Constants } from './constants';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+    title = Constants.TitleOfSite;
+    constructor() {
+        console.log(constants);
+    }
+    ngOnInit() {
+        console.log(this.title);
+    }
+}
